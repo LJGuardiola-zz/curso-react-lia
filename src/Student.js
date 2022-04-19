1f7f3dcdbf629cac4e13f552a3c267bf104990fb
Lucas Guardiola <ljguardiola@gmail.com>
2022-04-18 23:27:54 -0300

    Init

---
+++ src/Student.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Student.css'
+
+class Student extends Component {
+	render() {
+		let data = {
+			fistName: 'Lucas',
+			lastName: 'Guardiola',
+			age: 25
+		}
+		return (
+				<div className="student">
+					<p>{ data.fistName } { data.lastName }</p>
+					<p>{ data.age } años</p>
+				</div>
+		);
+	}
+}
+
+export default Student;
